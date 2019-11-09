aaaccb44a5e538b2bd9ae3eeb748b877be954be2
MarjoryeAlves <marjorye.lavigne@academico.ifpb.edu.br>
2019-11-09 11:22:43 -0200

    Tuplas

--- HerancaJPA/src/Pessoa.java
+++ HerancaJPA/src/Pessoa.java
@@ -8,6 +8,8 @@ import javax.persistence.Id;
 import javax.persistence.Inheritance;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipo", length = 1, discriminatorType = DiscriminatorType.STRING)
 @DiscriminatorValue("P")
 
 public class Pessoa implements Identificavel {
