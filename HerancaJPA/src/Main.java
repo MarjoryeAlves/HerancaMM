4d9b6f346c550f24f7f572297e6c02c098bdc499
Aluno <Aluno@LAB01-PC17>
2019-11-22 09:29:23 -0200

    FIM TC GRAÇAS A DEUS

--- HerancaJPA/src/Main.java
+++ HerancaJPA/src/Main.java
@@ -8,34 +8,34 @@ public class Main {
 		DAO<Tecnico> t = new DAO<Tecnico>();
 		DAO<Pessoa> pe = new DAO<Pessoa>();
 		
-		for (int i = 0; i <3000 ; i++) {
-			Aluno a = new Aluno();
-			a.setNome("");
-			a.setCpf(1212);
-			a.setEmail("");
-			d.save(a);
-			
-			Tecnico tec = new Tecnico();
-			tec.setNome("");
-			tec.setCpf(87463);
-			tec.setCurso("");
-			tec.setEmail("");
-			t.save(tec);
-			
-			Professor pr = new Professor();
-			pr.setNome("");
-			pr.setCpf(546);
-			pr.setEmail("");
-			pr.setCurso("");
-			prof.save(pr);
-			
-			Pessoa p = new Pessoa();
-			p.setNome("");
-			p.setEmail("");
-			p.setCpf(9874);
-			pe.save(p);
-			
-		}
+//		for (int i = 0; i <3000 ; i++) {
+//			Aluno a = new Aluno();
+//			a.setNome("");
+//			a.setCpf(1212);
+//			a.setEmail("");
+//			d.save(a);
+//			
+//			Tecnico tec = new Tecnico();
+//			tec.setNome("");
+//			tec.setCpf(87463);
+//			tec.setCurso("");
+//			tec.setEmail("");
+//			t.save(tec);
+//			
+//			Professor pr = new Professor();
+//			pr.setNome("");
+//			pr.setCpf(546);
+//			pr.setEmail("");
+//			pr.setCurso("");
+//			prof.save(pr);
+//			
+//			Pessoa p = new Pessoa();
+//			p.setNome("");
+//			p.setEmail("");
+//			p.setCpf(9874);
+//			pe.save(p);
+//			
+//		}
 		long fim = System.currentTimeMillis();
 		System.out.println(fim-inicio);
 		
